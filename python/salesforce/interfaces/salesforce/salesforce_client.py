3e2ec75c81fe960ac21934d7ecde65337993a607
JHare-rectangle <JHare@rectanglehealth.com>
2022-10-26 13:19:43 -0400

    Consolidated to one Salesforce library based on new capabilities Do retries on posting to SF endpoint Track time of each post

--- python/salesforce/interfaces/salesforce/salesforce_client.py
+++ python/salesforce/interfaces/salesforce/salesforce_client.py
@@ -1,14 +1,12 @@
-import pprint
-from salesforce_api import login, Salesforce as SalesforceApi
 from simple_salesforce import Salesforce
 from simple_salesforce.api import DEFAULT_API_VERSION
 
 
 class SalesforceClient:
-    def __init__(self, username, password, login_url, consumer_key, consumer_secret, endpoint):
+    def __init__(self, username, password, consumer_key, consumer_secret, endpoint, domain="login"):
         self._username = username
         self._password = password
-        self._login_url = login_url
+        self._domain = domain
         self._consumer_key = consumer_key
         self._consumer_secret = consumer_secret
         self._endpoint = endpoint
@@ -33,18 +31,6 @@ class SalesforceClient:
 
     def get_connection(self):
         if not self._salesforce:
-            # salesforce_api package supports oauth2 login like we used in the gateway-importer lambda but doesn't
-            #  appear to have a convenient Apex interface
-            sf_login = SalesforceApi(login.oauth2(username=self._username,
-                                                  password=self._password,
-                                                  client_id=self._consumer_key,
-                                                  client_secret=self._consumer_secret,
-                                                  instance_url=self._login_url))
-            self._access_token = sf_login.connection.access_token
-            self._instance_url = sf_login.connection.instance_url
-            self._session = sf_login.connection.session  # requests.sessions.Session object
-            self._version = sf_login.connection.version
-            # simple_salesforce package has the APEX support but not the login method we wanted.  But we have the
-            #  details from the above login to satisfy simple_salesforce from the salesforce_api login.
-            self._salesforce = Salesforce(session_id=self._access_token, instance_url=self._instance_url)
+            self._salesforce = Salesforce(username=self._username, password=self._password, domain=self._domain,
+                                          consumer_key=self._consumer_key, consumer_secret=self._consumer_secret)
         return self._salesforce
--- python/salesforce/reporting_server_to_sf.py
+++ python/salesforce/reporting_server_to_sf.py
@@ -1,7 +1,10 @@
+import datetime
 import math
 import os
 import pprint
 import pymssql
+import statistics
+import time
 from dotenv import load_dotenv
 from utils import format_sf_timestamp, time_string
 from salesforce.interfaces.salesforce.salesforce_client import SalesforceClient
@@ -9,12 +12,21 @@ from salesforce.interfaces.slack.slack_client import SlackClient
 
 done = []
 tables = []
+diffs = list()
 
 rows_per_page = 25
+initial_retry_wait = 30
+retry_backoff = 2
+max_retries = 3
 
 # XXX Error received when SF endpoint is overwhelmed
 """
 Update failed. First exception on row 0 with id 0013n00001uAbFGAA0; first error: CANNOT_INSERT_UPDATE_ACTIVATE_ENTITY, boomtownapp.AccountTrigger: execution of AfterUpdate caused by: System.AsyncException: Rate Limiting Exception : AsyncApexExecutions Limit exceeded.
+"""
+
+# ???
+"""
+Update failed. First exception on row 0 with id 0012S00002J10raQAB; first error: CANNOT_INSERT_UPDATE_ACTIVATE_ENTITY, boomtownapp.AccountTrigger: execution of AfterUpdate caused by: System.AsyncException: Database.executeBatch: batch apex job enqueue failed.
 """
 
 # New errors
@@ -41,23 +53,33 @@ def send_data_to_salesforce(data, salesforce_client):
             "sReportDate": f'{format_sf_timestamp(row["sReportDate"])}'
             if row.get("sReportDate") else ""
         })
-    resp = salesforce_client.post_data(body)
-    if resp:
-        if not isinstance(resp, dict):
-            raise RuntimeError(f"Unexpected response from Salesforce: {resp}")
-        return_tuple = int(resp.get("merchant_data_size", 0)), int(resp.get("updating_account_list_size", 0))
-        if return_tuple == (0, 0):
-            pprint.pprint(resp)
+    attempt = 1
+    wait_time = initial_retry_wait
+    while True:
+        d1 = datetime.datetime.now()
+        resp = salesforce_client.post_data(body)
+        delta = datetime.datetime.now() - d1
+        diffs.append(delta.total_seconds())
+        if resp is not None:
+            if not isinstance(resp, dict):
+                # raise RuntimeError(f"Unexpected response from Salesforce: {resp}")
+                print(f"\nUnexpected response from Salesforce: {resp}")
+            else:
+                # pprint.pprint(resp)
+                return int(resp.get("merchant_data_size", 0)), int(resp.get("updating_account_list_size", 0))
+            if attempt > max_retries:
+                raise RuntimeError(f"Salesforce request tried {max_retries + 1} times and always failed!")
+            print(f"Pausing {wait_time}s before retry {attempt}")
+            time.sleep(wait_time)
+            attempt += 1
+            wait_time *= retry_backoff
         else:
-            return return_tuple
+            # Only happens if data to post_data() is None or if the initial login returns None (which should not
+            #  be possible).  So we can quit now
+            break
     return 0, 0
-    # pprint.pprint(resp)
 
 
-# TODO - Maybe need to try harder to allow error messages to accumulate before final report
-# TODO - Probably need to have all lambdas part of the flow in a single repo to share Slack notification
-# TODO - How do batch POSTing lambdas report their counts of success?  How do retries work in there?  Who makes the
-#         final Slack report?
 def notify_slack(slack_client, table_name, rows_added_to_table, rows_to_be_sent_to_sf, rows_imported_by_sf,
                  pages_to_read, pages_read):
     base_fallback = "*Daily BP Transactions Report Ran*<!channel>"
@@ -79,7 +101,7 @@ def notify_slack(slack_client, table_name, rows_added_to_table, rows_to_be_sent_
             status_message = f"{status_message} ```{err_msg}```"
     else:
         icon_name = "white_check_mark"
-        status_message = "Report ran ```Successfully```"
+        status_message = "Report ran ```Successfully``` :ballmer:"
         fallback = f"{base_fallback} was successful"
     details = f"- Table created: `{table_name}`\n- Total rows added to the table: `{rows_added_to_table}`\n" \
               f"- Rows to be sent to SF: `{rows_to_be_sent_to_sf}`\n" \
@@ -96,14 +118,16 @@ if __name__ == "__main__":
     reporting_db_user = os.getenv("reporting_user")
     reporting_db_pw = os.getenv("reporting_password")
 
-    sf_consumer_key = os.getenv("salesForceConsumerKey")
-    sf_consumer_secret = os.getenv("salesForceConsumerSecret")
-    sf_endpoint = os.getenv("salesForceEndpoint")
-    sf_login_url = os.getenv("salesForceLoginUrl")
-    sf_user = os.getenv("salesForceUerName")
-    sf_password = os.getenv("salesForcePassword")
+    sf_consumer_key = os.getenv("salesforce_consumer_key")
+    sf_consumer_secret = os.getenv("salesforce_consumer_secret")
+    sf_endpoint = os.getenv("salesforce_endpoint")
+    # sf_login_url = os.getenv("salesforce_login_url")
+    sf_domain = os.getenv("salesforce_domain")
+    sf_user = os.getenv("salesforce_username")
+    sf_password = os.getenv("salesforce_password")
 
-    sf_client = SalesforceClient(sf_user, sf_password, sf_login_url, sf_consumer_key, sf_consumer_secret, sf_endpoint)
+    # sf_client = SalesforceClient(sf_user, sf_password, sf_login_url, sf_consumer_key, sf_consumer_secret, sf_endpoint)
+    sf_client = SalesforceClient(sf_user, sf_password, sf_consumer_key, sf_consumer_secret, sf_endpoint, sf_domain)
 
     conn = pymssql.connect(reporting_server, reporting_db_user, reporting_db_pw, reporting_db)
     cursor = conn.cursor(as_dict=True)
@@ -138,8 +162,8 @@ if __name__ == "__main__":
         if skip_yes_no.upper() == 'Y':
             table_count += 1
             cursor.execute(f"SELECT MID FROM BP_DAILY.dbo.{table_name}")
-            total_rows_added_to_table = len(cursor.fetchall()) # XXX "Total rows added to the table"
-            cursor.execute(f"SELECT MID FROM BP_DAILY.dbo.{table_name} group by MID")
+            total_rows_added_to_table = len(cursor.fetchall())  # XXX "Total rows added to the table"
+            cursor.execute(f"SELECT MID FROM BP_DAILY.dbo.{table_name} WHERE MID IS NOT NULL group by MID")
             total_rows_to_be_sent_to_sf = len(cursor.fetchall())  # XXX "Rows to be sent to SF"
             rows_imported_to_sf = 0
             pages_read = 0
@@ -152,34 +176,42 @@ if __name__ == "__main__":
                                 "SUM(CAST(MTDTransactionDollarVol as numeric(18,4))) as sMTDMerchantVolume, " \
                                 "SUM(CAST(YTDTransactionDollarVol as numeric(18,4))) as sYTDMerchantVolume, " \
                                 "SUM(CAST(YTDTransactionCount as numeric(18,4))) as sYTDMerchantTransaction " \
-                                f"FROM BP_DAILY.dbo.{table_name} group by MID,ReportDate ORDER BY MID DESC " \
-                                "OFFSET %d ROWS FETCH NEXT %d ROWS ONLY"
+                                f"FROM BP_DAILY.dbo.{table_name} WHERE MID IS NOT NULL "\
+                                "group by MID,ReportDate ORDER BY MID DESC OFFSET %d ROWS FETCH NEXT %d ROWS ONLY"
             # Simplified query that returns the same MIDs but skips everything extraneous in case something is screwing
             #  up the main query and I want to get some what data is involved in the issue
             # simple_query_str = "SELECT ReportDate as sReportDate, MID as sMerchantNumber " \
             #                    f"FROM BP_DAILY.dbo.{table_name} group by MID,ReportDate ORDER BY MID DESC " \
             #                    "OFFSET %d ROWS FETCH NEXT %d ROWS ONLY"
-            for curr_page in range(1, total_pages + 1):
+            for curr_page in range(615, total_pages + 1):
                 try:
+                    # print(f"{time_string()}: Fetching rows from database")
                     cursor.execute(general_query_str, ((curr_page - 1) * rows_per_page, rows_per_page))
                     rows = cursor.fetchall()
                     pages_read += 1
                     # print("")
                     # pprint.pprint(rows)
                     try:
+                        print(f"{time_string()}: Sending page {curr_page} of data to SF")
                         ds, us = send_data_to_salesforce(rows, sf_client)
                         rows_imported_to_sf += ds
-                        if ds != us:
-                            print(f"mds {ds} != uals {us} in page {curr_page} "
-                                  f"(MIDs {[r['sMerchantNumber'] for r in rows]})")
+                        # if ds != us:
+                        #     print(f"mds {ds} != uals {us} in page {curr_page} "
+                        #           f"(MIDs {[r['sMerchantNumber'] for r in rows]})")
                         merch_data_count += ds
                         update_count += us
                     except Exception as e:
                         print(f"Salesforce POST failed on page {curr_page}: {e}")
                 except Exception as e:
                     print(f"Reporting server query failed on page {curr_page}: {e}")
-            notify_slack(slack, table_name, total_rows_added_to_table, total_rows_to_be_sent_to_sf,
-                         rows_imported_to_sf, total_pages, pages_read)
+                if diffs:
+                    mean_diff = statistics.mean(diffs)
+                    print(f"{time_string()}:   Mean POST: {mean_diff}s  -  Per row time: {mean_diff / rows_per_page}s")
+                    print(f"{time_string()}:   Min  POST: {min(diffs)}s")
+                    print(f"{time_string()}:   Max  POST: {max(diffs)}s")
+                print(" ")
+            # notify_slack(slack, table_name, total_rows_added_to_table, total_rows_to_be_sent_to_sf,
+            #              rows_imported_to_sf, total_pages, pages_read)
         elif skip_yes_no.upper() == 'Q':
             skip_tables.extend(tables[t:])
             break
